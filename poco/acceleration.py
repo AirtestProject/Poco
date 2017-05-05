508d0329ee2fb1b54a171ec7e39cfdca37573ebe
李翔 <lxn3032@corp.netease.com>
2017-05-05 16:47:13 +0800

    修复一处死循环未做超时处理

--- poco/acceleration.py
+++ poco/acceleration.py
@@ -4,13 +4,15 @@ __author__ = 'lxn3032'
 
 import time
 
+from .exceptions import PocoTargetTimeout
+
 
 class PocoUIAccelerationMixin(object):
     """
     该mixin中定义一些常用的操作方法，将一些通用的逻辑封装起来。
     """
 
-    def dismiss(self, targets, exit_when=None, anchor='anchor', sleep_interval=1, appearance_timeout=20):
+    def dismiss(self, targets, exit_when=None, anchor='anchor', sleep_interval=1, appearance_timeout=20, timeout=120):
         """
         自动点掉目标对象，即一直点点到全都消失为止，适用于无脑点点点的界面。
 
@@ -19,13 +21,16 @@ class PocoUIAccelerationMixin(object):
         :param anchor: 点击对象的局部坐标系位置，默认点击anchor点。
         :param sleep_interval: 点击动作间隔，点了之后等待一段时间后再找下一个target进行点击
         :param appearance_timeout: 在进行dismiss之前会等待targets中任意一个target出现，超过了这个时间还没出现就自动退出了
+        :param timeout: dismiss阶段超时时长
         :return: None
         """
         try:
             self.wait_for_any(targets, timeout=appearance_timeout)
-        except RuntimeError:
+        except PocoTargetTimeout:
             # 仅当超时时自动退出
             return
+
+        start_time = time.time()
         while True:
             no_target = True
             for t in targets:
@@ -37,5 +42,5 @@ class PocoUIAccelerationMixin(object):
                         pass
             time.sleep(sleep_interval)
             should_exit = exit_when() if exit_when else False
-            if no_target or should_exit:
+            if no_target or should_exit or time.time() - start_time > timeout:
                 return
