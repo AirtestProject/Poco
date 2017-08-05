ef24bf00a7ad2ba57f289cac47a3c028f01857ec
lxn3032 <lxn3032@corp.netease.com>
2017-08-05 15:46:54 +0800

    漏处理默认参数

--- poco/assertions.py
+++ poco/assertions.py
@@ -9,31 +9,31 @@ __author__ = 'lxn3032'
 
 class PocoAssertionMixin(object):
     @staticmethod
-    def assert_equal(l, r, msg=''):
+    def assert_equal(l, r, msg=b''):
         if l != r:
             raise PocoAssertionError('断言失败于"{}". {} != {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
-    def assert_greater(l, r, msg=''):
+    def assert_greater(l, r, msg=b''):
         if l <= r:
             raise PocoAssertionError('断言失败于"{}". {} <= {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
-    def assert_greater_equal(l, r, msg=''):
+    def assert_greater_equal(l, r, msg=b''):
         if l <= r:
             raise PocoAssertionError('断言失败于"{}". {} < {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
-    def assert_less(l, r, msg=''):
+    def assert_less(l, r, msg=b''):
         if l <= r:
             raise PocoAssertionError('断言失败于"{}". {} >= {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
-    def assert_less_equal(l, r, msg=''):
+    def assert_less_equal(l, r, msg=b''):
         if l <= r:
             raise PocoAssertionError('断言失败于"{}". {} > {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
-    def assert_true(l, msg=''):
+    def assert_true(l, msg=b''):
         if l is not True:
             raise PocoAssertionError('断言失败于"{}". {} is not True.'.format(msg.decode('utf-8'), repr(l)))
