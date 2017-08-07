816567917a3367ac1d319d3c7c0ca0fd52dcd8c2
lxn3032 <lxn3032@corp.netease.com>
2017-08-07 21:54:36 +0800

    更换一种android的setText方式

--- poco/vendor/android/rpc.py
+++ poco/vendor/android/rpc.py
@@ -51,10 +51,12 @@ class AndroidRpcClient(RpcInterface):
 
     @transform_node_has_been_removed_exception
     def setattr(self, nodes, name, val):
+        self.remote_poco.attributor.setAttr(nodes, name, val)
         if name == 'text':
-            self.ime.text(val)
-        else:
-            self.remote_poco.attributor.setAttr(nodes, name, val)
+            check_val = self.getattr(nodes, 'text')
+            if check_val != val:
+                raise Exception('调用android uiautomator setText失败，希望设置"{}"，但得到"{}"'.encode('utf-8').format(val, check_val))
+            # self.ime.text(val)
 
     def select(self, query, multiple=False):
         return self.remote_poco.selector.select(query, multiple)
--- poco/vendor/android/test/common.py
+++ poco/vendor/android/test/common.py
@@ -18,8 +18,11 @@ class TextCommonCases(unittest.TestCase):
         with open('img.jpg', 'wb') as img:
             img.write(base64.b64decode(b64img))
 
+    def test_set_text(self):
+        self.poco('com.netease.my:id/netease_mpay__login_urs').set_text('adolli@163.com')
+
     def test_existence(self):
-        print self.poco('登录').exists()
+        print(self.poco('登录').exists())
 
     def test_existence_after_hide(self):
         btn = self.poco(text='快速游戏', type='android.widget.TextView').focus([0.5, -3])
@@ -32,4 +35,8 @@ class TextCommonCases(unittest.TestCase):
         self.assertEqual(btn2.exists(), True)
 
     def test_any(self):
-        self.poco(resourceIdMatches='^.*switch_account$').click()
+        btn = self.poco(text='快速游戏').focus([0.5, -3])
+        print btn.exists()
+        btn.click()
+        time.sleep(1)
+        print btn.exists()
