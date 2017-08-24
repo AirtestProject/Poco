8168b3dff116e603481193eb807c018bd63a45f4
lxn3032 <lxn3032@corp.netease.com>
2017-08-24 21:15:31 +0800

    加了基本的trace和main runner

---
+++ poco/unittest/result/trace.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+import sys
+import time
+
+
+def make_tracer(filename):
+    def tracer(frame, event, arg):
+        if event == 'line' and filename == frame.f_code.co_filename:
+            line_num = frame.f_lineno
+            # TODO 定一下标准格式
+            print(time.time(), line_num)
+        return tracer
+    return tracer
+
+
+def start_line_tracing(filename):
+    sys.settrace(make_tracer(filename))
