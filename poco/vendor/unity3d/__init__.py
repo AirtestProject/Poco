0cb657e37ceff97682016b5949a738096c0c6e4a
刘欣 <gzliuxin@corp.netease.com>
2017-09-18 14:40:15 +0800

    use dumper instead of dumpable

--- poco/vendor/unity3d/__init__.py
+++ poco/vendor/unity3d/__init__.py
@@ -4,18 +4,18 @@
 # @Date:   2017-07-14 19:47:51
 from poco import Poco
 from poco.agent import PocoAgent
-from poco.sdk.Dumpable import Dumpable
-from poco.vendor.localui.hierarchy import LocalUIHierarchy
+from poco.vendor.localui.hierarchy import LocalUIHierarchy, LocalUIDumper
 from poco.vendor.airtest.screen import AirtestScreen
 from poco.vendor.airtest.input import AirtestInput
 from poco.vendor.mh.mh_rpc import sync_wrapper
 from poco.vendor.mh.simplerpc.rpcclient import RpcClient, AsyncConn
 
 
-DEFAULT_ADDR = "ws://localhost:5003"
+DEFAULT_ADDR = ("localhost", 5003)
 
 
-class UnityPocoAgent(PocoAgent, Dumpable):
+class UnityPocoAgent(PocoAgent):
+
     def __init__(self, addr=DEFAULT_ADDR):
         # init airtest env
         from airtest.core.main import set_serialno
@@ -23,19 +23,26 @@ class UnityPocoAgent(PocoAgent, Dumpable):
         if not current_device():
             set_serialno()
 
-        self.conn = AsyncConn(DEFAULT_ADDR)
+        self.conn = AsyncConn(addr)
         self.c = RpcClient(self.conn)
         self.c.DEBUG = False
         self.c.run(backend=True)
 
-        hierarchy = LocalUIHierarchy(self)
+        hierarchy = LocalUIHierarchy(Dumper(self.c))
         screen = AirtestScreen()
         input = AirtestInput()
         super(UnityPocoAgent, self).__init__(hierarchy, input, screen, None)
 
+
+class Dumper(LocalUIDumper):
+
+    def __init__(self, rpcclient):
+        super(Dumper, self).__init__()
+        self.rpcclient = rpcclient
+
     @sync_wrapper
     def dumpHierarchy(self):
-        return self.c.call("dump")
+        return self.rpcclient.call("Dump")
 
 
 class UnityPoco(Poco):
