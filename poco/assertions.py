8e58ec3713b59c80265e53d46b8b5c40b2612979
lxn3032 <lxn3032@corp.netease.com>
2017-09-30 10:35:31 +0800

    poco自带的断言方法提示弃用

--- poco/assertions.py
+++ poco/assertions.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from poco.exceptions import PocoAssertionError
+from poco.utils.suppression import deprecated
 
 
 __author__ = 'lxn3032'
@@ -10,31 +11,37 @@ __author__ = 'lxn3032'
 # deprecated, use assertion method in unittest.case.PocoTestCase instead
 class PocoAssertionMixin(object):
     @staticmethod
+    @deprecated('use assertion method in unittest.case.PocoTestCase instead')
     def assert_equal(l, r, msg=b''):
         if l != r:
             raise PocoAssertionError('断言失败于"{}". {} != {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
+    @deprecated('use assertion method in unittest.case.PocoTestCase instead')
     def assert_greater(l, r, msg=b''):
         if l <= r:
             raise PocoAssertionError('断言失败于"{}". {} <= {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
+    @deprecated('use assertion method in unittest.case.PocoTestCase instead')
     def assert_greater_equal(l, r, msg=b''):
         if l <= r:
             raise PocoAssertionError('断言失败于"{}". {} < {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
+    @deprecated('use assertion method in unittest.case.PocoTestCase instead')
     def assert_less(l, r, msg=b''):
         if l <= r:
             raise PocoAssertionError('断言失败于"{}". {} >= {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
+    @deprecated('use assertion method in unittest.case.PocoTestCase instead')
     def assert_less_equal(l, r, msg=b''):
         if l <= r:
             raise PocoAssertionError('断言失败于"{}". {} > {}.'.format(msg.decode('utf-8'), repr(l), repr(r)))
 
     @staticmethod
+    @deprecated('use assertion method in unittest.case.PocoTestCase instead')
     def assert_true(l, msg=b''):
         if l is not True:
             raise PocoAssertionError('断言失败于"{}". {} is not True.'.format(msg.decode('utf-8'), repr(l)))
