a6493a16dc5ad9d8ae15b545ef2f054fbd0199f0
lxn3032 <lxn3032@corp.netease.com>
2017-11-02 18:24:04 +0800

    fix chinese readme

--- DocBuilder/build.bat
+++ DocBuilder/build.bat
@@ -3,7 +3,7 @@
 rmdir /S /Q source\
 mkdir source\
 copy /Y ..\README.rst source\
-copy /Y ..\README-CN.md source\
+copy /Y ..\README-CN.rst source\
 xcopy /Y /T ..\doc source\doc\
 xcopy /Y /S /E ..\doc source\doc\
 
@@ -11,11 +11,3 @@ xcopy /Y /S /E ..\doc source\doc\
 SET SPHINX_APIDOC_OPTIONS=members,show-inheritance
 sphinx-apidoc -Me -o source/ ../poco ../poco/utils/simplerpc
 sphinx-build -b html . ../doc-auto
-
-:: copy readme.md to doc-auto
-xcopy /Y /T ..\doc ..\doc-auto\source\source\doc\
-xcopy /Y /S /E ..\doc ..\doc-auto\source\source\doc\
-
-:: copy readme-built from .html to .md to ensure links are working well
-copy /Y ..\doc-auto\source\README.html ..\doc-auto\source\README.rst
-copy /Y ..\doc-auto\source\README-CN.html ..\doc-auto\source\README-CN.md
--- DocBuilder/build.sh
+++ DocBuilder/build.sh
@@ -12,11 +12,3 @@ cp -r ../doc source/
 export SPHINX_APIDOC_OPTIONS=members,show-inheritance
 sphinx-apidoc -Me -o source/ ../poco ../poco/utils/simplerpc
 sphinx-build -b html . ../doc-auto
-
-# copy readme.md to doc-auto
-mkdir ../doc-auto/source/source
-cp -r ../doc ../doc-auto/source/source
-
-# copy readme-built from .html to .md to ensure links are working well
-cp ../doc-auto/source/README.html ../doc-auto/source/README.rst
-cp ../doc-auto/source/README-CN.html ../doc-auto/source/README-CN.md
