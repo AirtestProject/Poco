e17f72313d1767c4a92e6115147cdc46194c1d2e
lxn3032 <lxn3032@corp.netease.com>
2017-11-09 11:06:13 +0800

    有个小bug

--- poco/utils/simplerpc/utils.py
+++ poco/utils/simplerpc/utils.py
@@ -8,6 +8,9 @@ def sync_wrapper(func):
         cb = func(*args, **kwargs)
         ret, err = cb.wait(timeout=5)
         if err:
+            # FIXME: 远端报错时，err为dict，此处无法抛出
+            # cb.wait里要处理这个err
+            print err['message']
             raise err
         return ret
     return new_func
