b1d01da581bf08d47d43ea21e465265673b6ed9c
lxn3032 <lxn3032@corp.netease.com>
2017-12-08 16:14:07 +0800

    add sphinx multil-language

--- DocBuilder/build.sh
+++ DocBuilder/build.sh
@@ -11,4 +11,10 @@ cp -r ../doc source/
 # exclude undoc members
 export SPHINX_APIDOC_OPTIONS=members,show-inheritance
 sphinx-apidoc -Me -o source/ ../poco ../poco/utils
+
+# international multi-languages
+sphinx-build -b gettext
+sphinx-intl update -p _build/gettext -l zh_CN
+
+export LANGUAGE=zh_CN
 sphinx-build -b html . ../doc-auto
--- DocBuilder/conf.py
+++ DocBuilder/conf.py
@@ -74,7 +74,9 @@ release = ''
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = os.environ.get('LANGUAGE') or 'zh_CN'  # language supported
+locale_dirs = ['locale/']  # path is example but recommended.
+gettext_compact = False  # optional.
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
