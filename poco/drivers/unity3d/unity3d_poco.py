f87697148bf59cf814177297497f6979401060eb
lxn3032 <lxn3032@corp.netease.com>
2018-07-11 12:53:38 +0800

    bug fix: u3d driver wrong class selector in UnityEditor mode

--- poco/drivers/unity3d/unity3d_poco.py
+++ poco/drivers/unity3d/unity3d_poco.py
@@ -43,7 +43,7 @@ class UnityPoco(StdPoco):
 
         dev = None
         if unity_editor:
-            dev = connect_device("Windows:///?class_name=UnityWndClass&title_re=Unity.*")
+            dev = connect_device("Windows:///?class_name=UnityContainerWndClass&title_re=Unity.*")
             game_window = dev.app.top_window().child_window(title="UnityEditor.GameView")
             dev._top_window = game_window.wrapper_object()
             dev.focus_rect = (0, 40, 0, 0)
