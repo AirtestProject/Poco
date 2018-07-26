d249144d24fb55a933e740d7a33699e57386e70f
林泽聪 <linzecong@corp.netease.com>
2018-07-26 19:29:40 +0800

    fix

--- poco/drivers/windows/windowsui_poco.py
+++ poco/drivers/windows/windowsui_poco.py
@@ -4,69 +4,55 @@ from poco.drivers.std import StdPoco
 from poco.utils.device import VirtualDevice
 from poco.drivers.std import DEFAULT_ADDR,DEFAULT_PORT
 from airtest.core.error import DeviceConnectionError
-import win32api
+import subprocess
 import time
 import os
-
+import atexit
 
 class WindowsPoco(StdPoco):
     """
     Poco WindowsUI implementation.
 
     Args:
-        selector (:py:obj:`dict`): find window by a selector, optional parameters: `name`,`handle`,`name_re`
-        name: find windows by name
+        selector (:py:obj:`dict`): find window by a selector, optional parameters: `title`,`handle`,`title_re`
+        title: find windows by title
         handle: find windows by handle
-        name_re: find windows by regular expression of name
+        title_re: find windows by regular expression of title
         addr (:py:obj:`tuple`): where the WindowsUI running on, (localhost,15004) by default
-        print_hierarchy (:py:obj:`bool`):  whether print the UI hierarchy in remote server,False by default
         options: see :py:class:`poco.pocofw.Poco`
 
     Examples:
-        
             from poco.drivers.windows import WindowsPoco
-            # poco = WindowsPoco({'name':'xxx'})
+            # poco = WindowsPoco({'title':'xxx'})
             # poco = WindowsPoco({'handle':123456})
-            # poco = WindowsPoco({'name_re':'[a-z][a-z][a-z]'}) # match the first matched window
+            # poco = WindowsPoco({'title_re':'[a-z][a-z][a-z]'}) # match the first matched window
 
     """
 
-    def __init__(self, selector={}, addr=DEFAULT_ADDR, print_hierarchy=False, **options):
+    def __init__(self, selector=None, addr=DEFAULT_ADDR, **options):
         if 'action_interval' not in options:
             options['action_interval'] = 0.5
 
-        if addr == DEFAULT_ADDR:
-            win32api.ShellExecute(0, 'open',os.path.dirname(__file__) + "\\sdk\\WindowsUI.py", '', '', 1)
+        if addr[0] == "localhost":
+            atexit.register(self.KillSDKProcess)
+            self.SDKProcess = subprocess.Popen("python " + os.path.dirname(__file__) + "\\sdk\\WindowsUI.py")
 
         dev = VirtualDevice(addr[0])
         super(WindowsPoco, self).__init__(addr[1], dev, False, **options)
 
-        if not selector:
+        cb = self.agent.rpc.call("ConnectWindow", selector)
+        ok = cb.wait(timeout=10)
+        if not ok[0]:
             raise DeviceConnectionError("Can't find any windows by the given parameter")
+        else:
+            self.agent.rpc.call("SetForeground")
 
-        if 'name' in selector:
-            cb = self.agent.rpc.call("ConnectWindowsByName", selector['name'], print_hierarchy)
-            ok = cb.wait(timeout=10)
-            if not ok[0]:
-                raise DeviceConnectionError("Can't find any windows by the given parameter")
-            else:
-                self.agent.rpc.call("SetForeground")
+    def KillSDKProcess(self):
+        time.sleep(2) # wait server respond
+        self.SDKProcess.kill()
+            
 
-        if 'handle' in selector:
-            cb = self.agent.rpc.call("ConnectWindowsByHandle", selector['handle'], print_hierarchy)
-            ok = cb.wait(timeout=10)
-            if not ok[0]:
-                raise DeviceConnectionError("Can't find any windows by the given parameter")
-            else:
-                self.agent.rpc.call("SetForeground")
-
-        if "name_re" in selector:
-            cb = self.agent.rpc.call("ConnectWindowsByNameRe", selector['name_re'], print_hierarchy)
-            ok = cb.wait(timeout=10)
-            if not ok[0]:
-                raise DeviceConnectionError("Can't find any windows by the given parameter")
-            else:
-                self.agent.rpc.call("SetForeground")   
+        
 
 
         
