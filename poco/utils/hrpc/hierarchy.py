3c7aed901dda00db76322af757c90daa572656e0
lxn3032 <lxn3032@corp.netease.com>
2018-07-31 20:17:17 +0800

    auto recover pocoservice for poco.drivers.android

--- poco/utils/hrpc/hierarchy.py
+++ poco/utils/hrpc/hierarchy.py
@@ -3,8 +3,11 @@
 # @Email:  gzliuxin@corp.netease.com
 # @Date:   2017-07-11 14:34:46
 
+from hrpc.exceptions import TransportDisconnected
+
 from poco.sdk.interfaces.hierarchy import HierarchyInterface
 from poco.utils.hrpc.utils import transform_node_has_been_removed_exception
+from poco.utils.retry import retries_when
 
 
 class RemotePocoHierarchy(HierarchyInterface):
@@ -15,16 +18,20 @@ class RemotePocoHierarchy(HierarchyInterface):
         self.attributor = attributor
 
     # node/hierarchy interface
+    @retries_when(TransportDisconnected, delay=3.0)
     @transform_node_has_been_removed_exception
     def getAttr(self, nodes, name):
         return self.attributor.getAttr(nodes, name)
 
+    @retries_when(TransportDisconnected, delay=3.0)
     @transform_node_has_been_removed_exception
     def setAttr(self, nodes, name, value):
         return self.attributor.setAttr(nodes, name, value)
 
+    @retries_when(TransportDisconnected, delay=3.0)
     def select(self, query, multiple=False):
         return self.selector.select(query, multiple)
 
+    @retries_when(TransportDisconnected, delay=3.0)
     def dump(self):
         return self.dumper.dumpHierarchy()
