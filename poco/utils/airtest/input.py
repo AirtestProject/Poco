61fa6d8cc9a925e2d935c1be2ff292f58c7d0e1c
chenjiyun <chenjiyun@corp.netease.com>
2019-07-26 16:23:42 +0800

    支持全面屏

--- poco/utils/airtest/input.py
+++ poco/utils/airtest/input.py
@@ -46,12 +46,21 @@ class AirtestInput(InputInterface):
         self._driver = driver
         self._driver.add_pre_action_callback(record_ui)
 
+    def get_target_pos(self, x, y):
+        """
+        get real time resolution on device of target (x,y)
+        """
+        offsetx, offsety, pw, ph = self._get_touch_resolution()
+        pos = [x * pw + offsetx, y * ph + offsety]
+        return pos
+
     def _get_touch_resolution(self):
         """
         get real time resolution on device if full screen
          or window size if running in window mode
         """
-        return current_device().get_current_resolution()
+        current_device().display_info
+        return current_device().get_render_resolution()
 
     def setTouchDownDuration(self, duration):
         self.default_touch_down_duration = duration
@@ -60,24 +69,21 @@ class AirtestInput(InputInterface):
         return self.default_touch_down_duration
 
     def click(self, x, y):
-        pw, ph = self._get_touch_resolution()
-        pos = [x * pw, y * ph]
+        pos = self.get_target_pos(x, y)
         touch(pos, duration=self.default_touch_down_duration)
 
     def swipe(self, x1, y1, x2, y2, duration=2.0):
         if duration <= 0:
             raise ValueError("Operation duration cannot be less equal 0. Please provide a positive number.")
         direction = x2 - x1, y2 - y1
-        pw, ph = self._get_touch_resolution()
-        p1 = [x1 * pw, y1 * ph]
+        pos = self.get_target_pos(x1, y1)
         steps = int(duration * 40) + 1
-        swipe(p1, vector=direction, duration=duration, steps=steps)
+        swipe(pos, vector=direction, duration=duration, steps=steps)
 
     def longClick(self, x, y, duration=2.0):
         if duration <= 0:
             raise ValueError("Operation duration cannot be less equal 0. Please provide a positive number.")
-        pw, ph = self._get_touch_resolution()
-        pos = [x * pw, y * ph]
+        pos = self.get_target_pos(x, y)
         touch(pos, duration=duration)
 
     def applyMotionEvents(self, events):
@@ -87,18 +93,19 @@ class AirtestInput(InputInterface):
         # Android minitouch only, currently
         from airtest.core.android.minitouch import DownEvent, MoveEvent, UpEvent, SleepEvent
 
-        w, h = self._get_touch_resolution()
         mes = []
         for e in events:
             t = e[0]
             if t == 'd':
                 contact = e[2]
                 x, y = e[1]
-                me = DownEvent([x * w, y * h], contact)
+                pos = self.get_target_pos(x, y)
+                me = DownEvent(pos, contact)
             elif t == 'm':
                 contact = e[2]
                 x, y = e[1]
-                me = MoveEvent([x * w, y * h], contact)
+                pos = self.get_target_pos(x, y)
+                me = MoveEvent(pos, contact)
             elif t == 'u':
                 contact = e[1]
                 me = UpEvent(contact)
