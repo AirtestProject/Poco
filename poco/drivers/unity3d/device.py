312f267b01b293f2486a1589411416f4ef799763
panhongtao <panhongtao@corp.netease.com>
2021-01-22 17:25:53 +0800

    fix:Fixed using unitypoco (unity_editor=True) can't connecting to unity editor.修复了使用UnityPoco(unity_editor=True)连接Unity编辑器时的错误
    The update of unity version results in the change of window naming standard, and the original regular expression cannot match.Unity版本更新导致窗口命名规范变动，原有的正则表达式无法匹配。
    
    date:2021-6-22 17:25:32
    (cherry picked from commit 4ec04f98f8946be3583b8af58777ea98fd88b354)

--- poco/drivers/unity3d/device.py
+++ poco/drivers/unity3d/device.py
@@ -4,7 +4,7 @@ from airtest.core.api import connect_device
 
 
 def UnityEditorWindow():
-    dev = connect_device("Windows:///?class_name=UnityContainerWndClass&title_re=Unity.*")
+    dev = connect_device("Windows:///?class_name=UnityContainerWndClass&title_re=.*Unity.*")
     game_window = dev.app.top_window().child_window(title="UnityEditor.GameView")
     dev._top_window = game_window.wrapper_object()
     dev.focus_rect = (0, 40, 0, 0)
