2d105ffa4893749864113a7e696feb4ea64c5f4d
张琦 <zhangqi2011@corp.netease.com>
2021-07-20 16:01:13 +0800

    fix poco doc
    (cherry picked from commit c5cf1620c6da9d1f0f562bc134f35eeac02a2846)

--- DocBuilder/conf.py
+++ DocBuilder/conf.py
@@ -34,7 +34,7 @@ print(sys.path)
 # compatible with readthedocs online builder and local builder
 # local: sphinx-build -b html . ../doc-auto
 # readthedocs: /bin/python -m sphinx -T -b html -d _build/doctrees
-if ('sphinx' in sys.argv or sys.argv[0].endswith('sphinx-build')) and ('html' in sys.argv or sys.argv[-1] == '_build/html'):
+if 'html' in sys.argv or sys.argv[-1] == '_build/html':
     if not os.path.exists('source'):
         os.mkdir('source')
 
