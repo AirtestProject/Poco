a50cb93112bac4f9bee51c57891fdef9217fed63
张琦 <zhangqi2011@corp.netease.com>
2022-04-06 11:50:09 +0800

    level up to 1.0.87
    (cherry picked from commit 4a487a4a4030045c71c9222ec5b656b0060cbd5c)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def parse_requirements(filename='requirements.txt'):
 
 setup(
     name='pocoui',
-    version='1.0.86',
+    version='1.0.87',
     keywords="poco, automation test, ui automation",
     description='Poco cross-engine UI automated test framework.',
     long_description='Poco cross-engine UI automated test framework. 2018 present by NetEase Games',
